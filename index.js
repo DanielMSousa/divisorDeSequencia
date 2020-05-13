8fb235d7cad356db8a47ea12431c41e6b27a32ce
danielmsousa <54556367+DanielMSousa@users.noreply.github.com>
2020-05-13 17:41:59 -0300

    Nova versão do site
    Nova versão do site com o design melhorado e funcionalidades adicionais

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 //FUNÇÕES
+function salvarDados(arr1, arr2, grupos, e){
+    localStorage.setItem('lista1', `${arr1}`);
+    localStorage.setItem('lista2', `${arr2}`);
+    localStorage.setItem('grupos', JSON.stringify(grupos));
+    localStorage.setItem('elementos', JSON.stringify(e));
+}
+
+function buscarDados(){
+    let lista1 = localStorage.getItem('lista1') || null;
+    let lista2 = localStorage.getItem('lista2') || null;
+    let grupos = JSON.parse(localStorage.getItem('grupos')) || null;
+    let elementos = JSON.parse(localStorage.getItem('elementos')) || null;
+    if(lista1 && lista2 && grupos && elementos){
+        mudaDom(lista1, lista2, grupos, elementos);
+    }
+}
+
 function formarSequencia(a, b){
     array = [];
     array.push(a);
@@ -71,9 +88,18 @@ function sequencia(numeros, n){
 
     let u1 = geraLista(novaLista1);
     let u2 = geraLista(novaLista2);
+    
+    salvarDados(u1, u2, numeroAgrupamentos, elementos);
+    mudaDom(u1, u2, numeroAgrupamentos, elementos);
+}
 
-    document.getElementById('lista1').innerHTML = `<span class="lista">Primeira lista:</span> ${u1} - (${numeroAgrupamentos[0]} grupos e ${elementos[0]} itens)`;
-    document.getElementById('lista2').innerHTML = `<span class="lista">Segunda lista:</span> ${u2} - (${numeroAgrupamentos[1]} grupos e ${elementos[1]} itens)`;
+function mudaDom(lista1, lista2, grupos, elementos){
+    document.getElementById('listas').style.display = "flex"
+
+    document.getElementById('lista1').innerHTML = `<span class="lista">Primeira lista:</span> ${lista1}`;
+    document.getElementById('lista2').innerHTML = `<span class="lista">Segunda lista:</span> ${lista2}`;
+    document.getElementById('dados1').innerHTML = `${grupos[0]} grupos e ${elementos[0]} itens`;
+    document.getElementById('dados2').innerHTML = `${grupos[1]} grupos e ${elementos[1]} itens`;
 }
 
 function receberDados(e){
@@ -89,6 +115,33 @@ function receberDados(e){
     else sequencia(lista, numero)
 }
 
+function copyText(element) {
+    var textToCopy = element.innerText;
+    textoVerdade = textToCopy.replace(/,/g, '');
+    textoVerdade = textoVerdade.split(" ").splice(2);
+
+    var myTemporaryInputElement = document.createElement('input');
+    myTemporaryInputElement.type = 'text';
+    myTemporaryInputElement.value = textoVerdade;
+    
+    document.body.appendChild(myTemporaryInputElement);
+    
+    myTemporaryInputElement.select();
+    document.execCommand('Copy');
+    
+    document.body.removeChild(myTemporaryInputElement);
+   }
+
 //INTERAÇÃO COM A DOM
+//Cálcular os dados...
 const botao = document.getElementById('botao');
 botao.addEventListener('click', receberDados);
+
+const lista1 = document.getElementById('lista1');
+const lista2 = document.getElementById('lista2');
+const copiar1 = document.getElementById('copia1');
+const copiar2 = document.getElementById('copia2');
+copiar1.addEventListener('click', () => copyText(lista1));
+copiar2.addEventListener('click', () => copyText(lista2));
+
+window.onload = buscarDados();
